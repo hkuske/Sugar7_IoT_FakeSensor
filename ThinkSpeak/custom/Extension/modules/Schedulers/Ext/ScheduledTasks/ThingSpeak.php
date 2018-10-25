4e4a6f9e24558698a29a4c2375c5e24a795ba9dc
Harald Kuske <h.kuske@gmx.net>
2018-10-25 18:41:27 +0200

    Update ThinkSpeak/custom/Extension/modules/Schedulers/Ext/ScheduledTasks/ThingSpeak.php

--- ThinkSpeak/custom/Extension/modules/Schedulers/Ext/ScheduledTasks/ThingSpeak.php
+++ ThinkSpeak/custom/Extension/modules/Schedulers/Ext/ScheduledTasks/ThingSpeak.php
@@ -34,6 +34,8 @@ function ThingSpeak() {
 //  Version: 
     $GLOBALS['log']->info("----->Scheduler fired job of type ThingSpeak()");
 
+    $api_key = "xxxxxxxxxxxxxxxx";
+	
     $typ = "GET";
     $typ = "POST";
 
@@ -48,20 +50,16 @@ function ThingSpeak() {
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);  // wichtig
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv: 1.9.0.8) Gecko/2009032609 Firefox/3.0.8 (.NET CLR 3.5.30729)');
 
-    if ($typ=="GET")
+    $par = "api_key=".$api_key;
+	$par.= "&field1=".$temperatur;
+    $par.= "&field2=".$drehzahl;
+
+	if ($typ=="GET")
     {	
-    	$url.= "?api_key=WKX5LK0XZ9URVNJ6";
-    	$url.= "&field1=".$temperatur;
-    	$url.= "&field2=".$drehzahl;
+    	$url.= "?" . $par;
     }else{
-        $params = array(
-    	   "api_key" => "WKX5LK0XZ9URVNJ6",
-    	   "field1" => $temperatur,
-           "field2" => $drehzahl,
-    	);
         curl_setopt($ch, CURLOPT_POST, 1);
-        $jsonEncodedData = json_encode($params);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonEncodedData);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $par);
     }	
 
 	curl_setopt($ch, CURLOPT_URL, $url);
@@ -71,8 +69,9 @@ function ThingSpeak() {
 		$CURLContent = $data;
 	}
 	curl_close($ch);
-//	$XMLContent = explode("\n",$CURLContent);
-	$GLOBALS['log']->info("CURLContent=".print_r($CURLContent,true));
+	
+	$jsonContent = json_decode($CURLContent);
+	$GLOBALS['log']->info("jsonContent=".print_r($jsonContent,true));
 	
     $GLOBALS['log']->info("----->Scheduler ended job of type ThingSpeak()");
 

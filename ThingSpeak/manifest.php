ad80b489949c42c6981033a7100a9ce274d09ec7
Harald Kuske <h.kuske@gmx.net>
2019-05-04 08:04:45 +0200

    9.0.0 Intallable

---
+++ ThingSpeak/manifest.php
@@ -0,0 +1,43 @@
+<?php
+/*
+  IoT Fake Sensor for ThingSpeak
+ */
+
+$manifest = array (
+  'acceptable_sugar_versions' => 
+  array (
+    0 => '7.*.*',
+    1 => '8.*.*',
+    2 => '9.*.*',
+  ),
+  'acceptable_sugar_flavors' => 
+  array (
+    0 => 'PRO',
+    1 => 'CORP',
+    2 => 'ENT',
+    3 => 'ULT',
+  ),
+  'readme' => '',
+  'key' => '',
+  'author' => 'kuske',
+  'description' => 'Scheduler: ThingSpeak_Fake',
+  'icon' => '',
+  'is_uninstallable' => true,
+  'name' => 'Currency_Update',
+  'published_date' => '2019-05-01 00:00:00',
+  'type' => 'module',
+  'version' => 9.0.0,
+  'remove_tables' => 'false',
+);
+
+$installdefs = array (
+  'id' => 'ThingSpeak_Fake',
+  'copy' => 
+  array (
+    0 => 
+    array (
+      'from' => '<basepath>/custom/',
+      'to' => 'custom/',
+    ),
+  ),
+);
